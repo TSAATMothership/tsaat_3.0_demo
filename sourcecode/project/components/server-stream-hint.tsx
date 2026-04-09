ae6b59394433adef3c7b60de4d1bcf26184dd349
Shuffydog <contactseaunlyon@gmail.com>
2026-04-09 21:06:53 +1000

    Cleaned up directory REPO folders and files

--- sourcecode/project/components/server-stream-hint.tsx
+++
@@ -1,4 +0,0 @@
-export async function ServerStreamHint() {
-  await Promise.resolve();
-  return null;
-}
--- sourcecode/project/lib/actions.ts
+++
@@ -1,6 +0,0 @@
-import { SpiId } from "@/lib/types";
-import { SPI_ACTIONS } from "@/lib/spi-metadata";
-
-export function recommendedAction(spiId: SpiId): string {
-  return SPI_ACTIONS[spiId];
-}
