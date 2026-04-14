a0cb66cd97078c97ca2be6536f0c25da65a9b472
Shuffydog <contactseaunlyon@gmail.com>
2026-04-14 10:10:49 +1000

    udpate

--- components/menu-navigation.tsx
+++ components/menu-navigation.tsx
@@ -109,26 +109,6 @@ export function MenuNavigation({ items }: { items: MenuItem[] }) {
   }, []);
 
   useEffect(() => {
-    if (!isMounted || !isOperationsMenuOpen) {
-      return;
-    }
-
-    const previousOverflow = document.body.style.overflow;
-    const previousPaddingRight = document.body.style.paddingRight;
-    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-
-    document.body.style.overflow = "hidden";
-    if (scrollbarWidth > 0) {
-      document.body.style.paddingRight = `${scrollbarWidth}px`;
-    }
-
-    return () => {
-      document.body.style.overflow = previousOverflow;
-      document.body.style.paddingRight = previousPaddingRight;
-    };
-  }, [isMounted, isOperationsMenuOpen]);
-
-  useEffect(() => {
     if (!isOperationsMenuOpen) {
       return;
     }
